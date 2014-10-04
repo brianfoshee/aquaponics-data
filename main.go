27b102afbecbd3448520f2df1621e5241af5bb94
Brian Foshee <brianfoshee@gmail.com>
2014-10-04 15:03:24 -0400

    json decoding

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"os"
+	"time"
 )
+
+type Reading struct {
+	PH          float64 `json:"ph"`
+	TDS         float64 `json:"tds"`
+	Temperature float64 `json:"temperature"`
+	CreatedAt   time.Time
+}
 
 func main() {
 	http.HandleFunc("/", hello)
-	fmt.Println("Listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
@@ -16,5 +23,19 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "HELLO!")
+	reading := new(Reading)
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&reading); err != nil {
+		panic(err)
+	}
+	reading.CreatedAt = time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	data, err := json.Marshal(reading)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(data)
 }
