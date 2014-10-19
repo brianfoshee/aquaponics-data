2f365c495f80d6ea8b8e11247a301f3307ba6898
letsgitgrowing <nathanpreseault@gmail.com>
2014-10-19 14:31:42 -0400

    Changed "GET" request for /readings/latest to /readings?number=1

--- frontend/main.js
+++ frontend/main.js
@@ -1,34 +1,33 @@
 window.onload = function() {
      refreshPage();
-     setInterval(refreshPage, 5000);}
+     setInterval(refreshPage, 60000);}
 
-function refreshPage()
-{
+function refreshPage() {
 	gaugeRequest = serverRequest();
-	var URL = "http://gowebz.herokuapp.com/readings/latest";
+	var URL = "http://localhost:5000/readings?number=1";
 	gaugeRequest.open("GET", URL, true);
-    gaugeRequest.onreadystatechange = function()
-    {
-		if (gaugeRequest.readyState == 4 && gaugeRequest.status == 200)
-      {
-        JSONData = JSON.parse(gaugeRequest.responseText);
-        var waterTempData = JSONData.temperature;
-        var phData = JSONData.ph;
-        var tdsData = JSONData.tds;
-        var Time = new Date(JSONData.created_at).format("H:i");
+    gaugeRequest.onreadystatechange = function() {
+    	if (gaugeRequest.readyState == 4 && gaugeRequest.status == 200) {
+    		JSONData = JSON.parse(gaugeRequest.responseText);
+    		var waterTempData = JSONData[0].temperature;
+    		var phData = JSONData[0].ph;
+    		var tdsData = JSONData[0].tds;
+    		var Time = new Date(JSONData[0].created_at).format("H:i");
 
-        updateCharts(Time, waterTempData, phData, tdsData);
-        updateGauges(waterTempData, phData, tdsData);
-      }
-	}
+    		updateCharts(Time, waterTempData, phData, tdsData);
+    		updateGauges(waterTempData, phData, tdsData);
+    		}
+    	}
     gaugeRequest.send(null);
 }
 
-function serverRequest()
-{
-	try { var newRequest = new XMLHttpRequest(); }
+function serverRequest() {
+	try { 
+		var newRequest = new XMLHttpRequest(); 
+	}
 	catch (requestError){
 		alert("Exception caught in Charts.serverRequest()");
-		var newRequest=null;}
+		var newRequest=null;
+	}
 	return(newRequest)
 }
