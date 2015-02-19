5fae9aa25c59e9e5d0086911a9bbea6156621b7b
Nate Prayzo <nathanpreseault@gmail.com>
2015-02-19 14:41:37 -0500

    Split gauges into seprate functions and set proper parameters

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -3,7 +3,9 @@ google.load("visualization", "1", {packages:["gauge", "corechart"]});
 google.setOnLoadCallback(initVisualizations);
 
 function initVisualizations() {
-  drawGauges();
+  drawPhGauge();
+  drawTdsGauge();
+  drawWaterTempGauge();
   drawChart();
 }
 
@@ -53,23 +55,72 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-function drawGauges() {
+function drawPhGauge() {
+
+  var gauge = new google.visualization.Gauge(document.getElementById('ph-gauge'))
   var data = google.visualization.arrayToDataTable([
     ['Label', 'Value'],
-    ['Memory', 80],
+    ['PH', 5.5],
+  ]);
+  
+  var options = {
+	min: 4,
+	max: 7,
+	                                      //Red
+	greenFrom: 4, greenTo: 7, greenColor: "#FF0000",
+				              //Yellow
+	yellowFrom: 4.5, yellowTo: 6.5, yellowColor: "#FFFF00",
+			                      //Green		
+	redFrom: 5, redTo: 6, redColor: "#00FF00",
+	majorTicks: [4,4.5,5,5.5,6,6.5,7],
+	minorTicks: 5,
+	animation:{duration: 2000, easing: 'linear'}
+  };
+  gauge.draw(data, options);
+
+}
+function drawTdsGauge() {
+  var gauge = new google.visualization.Gauge(document.getElementById('tds-gauge'))
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['TDS', 1000],
   ]);
 
   var options = {
-    redFrom: 90, redTo: 100,
-    yellowFrom:75, yellowTo: 90,
-    minorTicks: 5
+	min: 0,
+	max: 2000,
+	                                      //Red
+	greenFrom: 0, greenTo: 2000, greenColor: "#FF0000",
+				              //Yellow
+	yellowFrom: 250, yellowTo: 1750, yellowColor: "#FFFF00",
+			                      //Green		
+	redFrom: 500, redTo: 1500, redColor: "#00FF00",
+	majorTicks: ['0',500,1000,1500,2000],
+	minorTicks: 5,
+	animation:{duration: 2000, easing: 'linear'}
   };
 
-  var phChart = new google.visualization.Gauge(document.getElementById('ph-gauge')),
-    tdsChart = new google.visualization.Gauge(document.getElementById('tds-gauge')),
-    wTempChart = new google.visualization.Gauge(document.getElementById('wtemp-gauge'));
+  gauge.draw(data, options);
 
-  phChart.draw(data, options);
-  tdsChart.draw(data, options);
-  wTempChart.draw(data, options);
+}
+function drawWaterTempGauge() {
+  var gauge = new google.visualization.Gauge(document.getElementById('wtemp-gauge'));
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['H20 Temp', 75],
+  ]);
+  var options = {
+	min: 50,
+	max: 100,
+	                                      //Red
+	greenFrom: 50, greenTo: 100, greenColor: "#FF0000",
+				              //Yellow
+	yellowFrom: 60, yellowTo: 90, yellowColor: "#FFFF00",
+			                      //Green		
+	redFrom: 70, redTo: 80, redColor: "#00FF00",
+	majorTicks: [50,60,70,80,90,100],
+	minorTicks: 10,
+	animation:{duration: 2000, easing: 'linear'}
+  };
+  gauge.draw(data,options)
 }
