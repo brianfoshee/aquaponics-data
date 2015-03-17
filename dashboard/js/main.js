50594dd3172cf717fe73dd1575b41c6868c47356
Brian Foshee <brianfoshee@gmail.com>
2015-03-16 22:18:28 -0400

    update chart with new data

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -6,7 +6,7 @@ function initVisualizations() {
   initPhGauge();
   initTdsGauge();
   initWaterTempGauge();
-  drawChart();
+  initChart();
   updateGauges();
 }
 
@@ -23,14 +23,14 @@ function updateGauges() {
     timestamps.sort().reverse();
     var first = timestamps[0];
     var readings = data[first];
-    console.log(first, readings);
-
     var ph = $('#ph-gauge').data('data');
     var tds = $('#tds-gauge').data('data');
     var wtemp = $('#wtemp-gauge').data('data');
+
     drawGauge($('#ph-gauge'), ph(readings.ph));
     drawGauge($('#tds-gauge'), tds(readings.tds));
     drawGauge($('#wtemp-gauge'), wtemp(readings.water_temperature));
+    updateChart(data);
   });
   req.fail(function(){
     console.log("failed");
@@ -42,6 +42,20 @@ function drawGauge(ele, data) {
   var gauge = $(ele).data("gauge");
   var options = $(ele).data("options");
   gauge.draw(data, options);
+}
+
+function updateChart(readings) {
+  var ele = $('#all-chart');
+  $(ele).data('readings', readings);
+  var timestamps = Object.keys(readings);
+  timestamps.sort();
+  var data = $(ele).data('data');
+  var chart = $(ele).data('chart');
+  var options = $(ele).data('options');
+  var arr = $.map(timestamps, function(v,i){
+    return [[v, readings[v].ph]];
+  });
+  chart.draw(data(arr), options);
 }
 
 function initPhGauge() {
@@ -126,48 +140,31 @@ function initWaterTempGauge() {
   gauge.draw(data(72),options)
 }
 
-function drawChart() {
+function initChart() {
+  var data = function(readings){
+    var d = new google.visualization.DataTable();
+    d.addColumn('string', 'Timestamp');
+    d.addColumn('number', 'Ph');
+    d.addRows(readings);
+    return d;
+  }
 
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'X');
-  data.addColumn('number', 'Dogs');
-
-  data.addRows([
-    [0, 0],   [1, 10],  [2, 23],
-    [3, 17],  [4, 18],  [5, 9],
-    [6, 11],  [7, 27],  [8, 33],
-    [9, 40],  [10, 32], [11, 35],
-    [12, 30], [13, 40], [14, 42],
-    [15, 47], [16, 44], [17, 48],
-    [18, 52], [19, 54], [20, 42],
-    [21, 55], [22, 56], [23, 57],
-    [24, 60], [25, 50], [26, 52],
-    [27, 51], [28, 49], [29, 53],
-    [30, 55], [31, 60], [32, 61],
-    [33, 59], [34, 62], [35, 65],
-    [36, 62], [37, 58], [38, 55],
-    [39, 61], [40, 64], [41, 65],
-    [42, 63], [43, 66], [44, 67],
-    [45, 69], [46, 69], [47, 70],
-    [48, 72], [49, 68], [50, 66],
-    [51, 65], [52, 67], [53, 70],
-    [54, 71], [55, 72], [56, 73],
-    [57, 75], [58, 70], [59, 68],
-    [60, 64], [61, 60], [62, 65],
-    [63, 67], [64, 68], [65, 69],
-    [66, 70], [67, 72], [68, 75],
-    [69, 80]
-  ]);
+  var readings = [];
 
   var options = {
     hAxis: {
       title: 'Time'
     },
     vAxis: {
-      title: 'Popularity'
+      title: 'Ph'
     }
   };
+  var ele = $('#all-chart');
+  var chart = new google.visualization.LineChart(ele[0]);
 
-  var chart = new google.visualization.LineChart(document.getElementById('all-chart'));
-  chart.draw(data, options);
+  $(ele).data("chart", chart);
+  $(ele).data("options", options);
+  $(ele).data('data', data);
+
+  chart.draw(data(readings), options);
 }
